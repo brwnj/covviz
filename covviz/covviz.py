fc7782b26e91d2f62988833db6026957bf66a5cc
Joe Brown <brwnjm@gmail.com>
2019-08-09 10:26:16 -0600

    supports custom ped

--- covviz/covviz.py
+++ covviz/covviz.py
@@ -17,6 +17,107 @@ logging.basicConfig(level=logging.INFO, format="[%(levelname)s] %(message)s")
 logger = logging.getLogger("covviz")
 
 
+def parse_args():
+    p = argparse.ArgumentParser(
+        description=__doc__, formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+    # this will later be the only requirement
+    p.add_argument(
+        "bed",
+        help=(
+            "bed3+ file format with a header defining sample columns "
+            "after chrom, start, and end."
+        ),
+    )
+
+    # TODO calculate from bed
+    p.add_argument("roc", help="indexcov .roc output")
+
+    p.add_argument(
+        "-e",
+        "--exclude",
+        default="^GL|^hs|^chrEBV$|M$|MT$|^NC|_random$|Un_|^HLA\-|_alt$|hap\d+$",
+        help="chromosome regex to exclude from analysis",
+    )
+    p.add_argument(
+        "-x",
+        "--sex-chroms",
+        default="X,Y",
+        help="sex chromosomes as they are defined in your bed, e.g. chrX,chrY",
+    )
+
+    p.add_argument(
+        "-z",
+        "--z-threshold",
+        default=3.5,
+        type=float,
+        help=(
+            "the point at which we determine a sample is an outlier "
+            "from the group at any given point"
+        ),
+    )
+    p.add_argument(
+        "-d",
+        "--distance-threshold",
+        default=150000,
+        type=int,
+        help=(
+            "when calculating significance, the Z-score has to be "
+            "above the threshold for consecutive points up to the "
+            "total distance set by distance threshold"
+        ),
+    )
+    p.add_argument(
+        "-s",
+        "--slop",
+        default=500000,
+        type=int,
+        help=(
+            "slop is the distance to add to traces when plotting -- "
+            "without slop, it's not always clear what happens to the "
+            "points immediately flanking the area of significant "
+            "deviation"
+        ),
+    )
+    p.add_argument(
+        "--gff",
+        help=(
+            ".gff reference file to place an optional gene track; "
+            "only rows of type 'gene' are used and annotated with "
+            "gene ID where the attributes include 'Name=<symbol>;' "
+            "annotation"
+        ),
+    )
+
+    p.add_argument(
+        "-o", "--output", default="covviz_report.html", help="output file path"
+    )
+    p.add_argument(
+        "--min-samples",
+        default=8,
+        type=int,
+        help=(
+            "show all traces when analyzing this few samples; ignores "
+            "z-threshold, distance-threshold, and slop"
+        ),
+    )
+
+    meta_group = p.add_argument_group("sample metadata")
+    meta_group.add_argument(
+        "-p", "--ped", help="ped file defining samples, sex, and other metadata"
+    )
+    meta_group.add_argument(
+        "--sample-col",
+        default="sample_id",
+        help="when using --ped, this defines the sample ID column",
+    )
+    meta_group.add_argument(
+        "--sex-col", default="sex", help="when using --ped, this defines the sex column"
+    )
+
+    return p.parse_args()
+
+
 # TODO calculate this based on the bed
 def parse_roc(path, traces, samples):
     chroms = list(traces.keys())
@@ -59,84 +160,43 @@ def compress_data(data):
     return LZString().compressToBase64(json_str)
 
 
-def parse_args():
-    p = argparse.ArgumentParser(
-        description=__doc__, formatter_class=argparse.ArgumentDefaultsHelpFormatter
-    )
-    # this will later be the only requirement
-    p.add_argument("bed", help="indexcov .bed output")
-
-    p.add_argument(
-        "--exclude",
-        default="^GL|^hs|^chrEBV$|M$|MT$|^NC|_random$|Un_|^HLA\-|_alt$|hap\d+$",
-        help="chromosome regex to exclude from analysis",
-    )
-    p.add_argument("--ped", help="indexcov .ped output")
-    p.add_argument(
-        "--sex-chroms", default="X,Y", help="sex chromosomes, e.g. chrX,chrY"
-    )
-    p.add_argument(
-        "--z-threshold",
-        default=3.5,
-        type=float,
-        help="the point at which we determine a sample is an outlier from the group at any given point",
-    )
-    p.add_argument(
-        "--distance-threshold",
-        default=150000,
-        type=int,
-        help="when calculating significance, the Z-score has to be above the threshold for consecutive points up to the total distance set by distance threshold",
-    )
-    p.add_argument(
-        "--slop",
-        default=500000,
-        type=int,
-        help="slop is the distance to add to traces when plotting -- without slop, it's not always clear what happens to the points immediately flanking the area of significant deviation",
-    )
-    p.add_argument("--gff", help=".gff reference file")
-    p.add_argument("--roc", help="indexcov .roc output")
-    p.add_argument("--output", default="covviz_report.html", help="output file path")
-    p.add_argument(
-        "--min-samples",
-        default=6,
-        type=int,
-        help="show all traces when analyzing this few samples; ignores z-threshold, distance-threshold, and slop",
-    )
-    return p.parse_args()
-
-
 def cli():
     args = parse_args()
 
-
     env = Environment(
-        loader=FileSystemLoader(os.path.join(os.path.dirname(__file__), 'templates')), autoescape=select_autoescape(["html"])
+        loader=FileSystemLoader(os.path.join(os.path.dirname(__file__), "templates")),
+        autoescape=select_autoescape(["html"]),
     )
 
     exclude = args.exclude.replace("~", "").replace(",", "|")
-    sample_col = "sample_id"
-    sex_col = "sex"
 
     logger.info("parsing bed file (%s)" % args.bed)
     traces, samples = parse_bed(
         args.bed,
         args.exclude,
         args.ped,
-        sample_col,
-        sex_col,
+        args.sample_col,
+        args.sex_col,
         args.sex_chroms,
         args.z_threshold,
         args.distance_threshold,
         args.slop,
         args.min_samples,
     )
+
     logger.info("parsing gff file (%s)" % args.gff)
     traces = parse_gff(args.gff, traces)
+
     logger.info("parsing roc file (%s)" % args.roc)
     traces = parse_roc(args.roc, traces, samples)
-    traces = parse_ped(args.ped, traces, sample_col, args.sex_chroms)
+
+    if args.ped:
+        logger.info("parsing ped file (%s)" % args.ped)
+        traces = parse_ped(args.ped, traces, args.sample_col, args.sex_chroms)
+
     with open(args.output, "w") as fh:
         compressed_json_data = compress_data(traces)
         html_template = env.get_template("covviz.html")
         print(html_template.render(data=compressed_json_data), file=fh)
+
     logger.info("processing complete")
--- covviz/ped.py
+++ covviz/ped.py
@@ -4,65 +4,95 @@ from collections import defaultdict
 from .utils import gzopen
 
 
+def check_ped_header(header, cols):
+    """
+    header (list) - current ped header columns
+    cols (list) - typical columns we'll see from indexcov
+    """
+    header = set(header)
+    cols = set(cols)
+    is_indexcov = True
+    if len(cols - header) > 0:
+        is_indexcov = False
+    return is_indexcov
+
+
 def parse_ped(path, traces, sample_col, sex_chroms):
     table_data = list()
     ped_data = dict(
         inferred=defaultdict(list), bins=defaultdict(list), pca=defaultdict(list)
     )
     sex_chroms = [i.strip() for i in sex_chroms.split(",")]
+
+    # indexcov .ped format
+    required_vals = [sample_col, "bins.in", "bins.out", "bins.lo"]  # CNX, CNY or CNchrX, CNchrY
     float_vals = ["slope", "p.out", "PC1", "PC2", "PC3", "PC4", "PC5"]
-    int_vals = ["sex", "bins.out", "bins.lo", "bins.hi", "bins.in"]
+    int_vals = ["bins.out", "bins.lo", "bins.hi", "bins.in"]
+
+    via_indexcov = False
 
     with gzopen(path) as fh:
         header = fh.readline().strip().split("\t")
+        # datatables needs these escaped
         datatable_cols = [dict(title=i, data=i.replace(".", "\\.")) for i in header]
         table_data.append(datatable_cols)
         fh.seek(0)
         reader = csv.DictReader(fh, delimiter="\t")
+
+        via_indexcov = check_ped_header(header, required_vals)
+
         for row in reader:
 
             # coerce vals
-            for k, v in row.items():
-                if k in int_vals:
-                    row[k] = int(v)
-                elif k in float_vals:
-                    row[k] = float(v)
-                elif k.startswith("CN"):
-                    row[k] = float(v)
+            if via_indexcov:
+                for k, v in row.items():
+                    if k in int_vals:
+                        row[k] = int(v)
+                    elif k in float_vals:
+                        row[k] = float(v)
+                    elif k.startswith("CN"):
+                        row[k] = float(v)
 
             # coerced vals saved in data.ped
             table_data.append(row)
-            # inferred sex
-            ped_data["inferred"]["x"].append(row["CN%s" % sex_chroms[0]])
-            try:
-                ped_data["inferred"]["y"].append(row["CN%s" % sex_chroms[1]])
-            except IndexError:
-                ped_data["inferred"]["y"].append(0)
-            # male
-            if row["sex"] == 1:
-                ped_data["inferred"]["color"].append("rgba(12,44,132,0.5)")
-                ped_data["inferred"]["hover"].append(
-                    "Sample: %s<br>Inferred X CN: 1" % (row["sample_id"],)
-                )
-            else:
-                ped_data["inferred"]["color"].append("rgba(227,26,28,0.5)")
-                ped_data["inferred"]["hover"].append(
-                    "Sample: %s<br>Inferred X CN: 2" % (row["sample_id"],)
-                )
-            # bin plot
-            total = row["bins.in"] + row["bins.out"]
-            ped_data["bins"]["samples"].append(row["sample_id"])
-            ped_data["bins"]["x"].append(row["bins.lo"] / total)
-            ped_data["bins"]["y"].append(row["bins.out"] / total)
-            # PCAs
-            try:
-                ped_data["pca"]["pca_1"].append(row["PC1"])
-                ped_data["pca"]["pca_2"].append(row["PC2"])
-                ped_data["pca"]["pca_3"].append(row["PC3"])
-            except KeyError:
-                pass
+
+            if via_indexcov:
+                # inferred sex
+                ped_data["inferred"]["x"].append(row["CN%s" % sex_chroms[0]])
+                try:
+                    ped_data["inferred"]["y"].append(row["CN%s" % sex_chroms[1]])
+                except IndexError:
+                    ped_data["inferred"]["y"].append(0)
+
+                # male
+                if row["sex"] == 1:
+                    ped_data["inferred"]["color"].append("rgba(12,44,132,0.5)")
+                    ped_data["inferred"]["hover"].append(
+                        "Sample: %s<br>Inferred X CN: 1" % (row[sample_col],)
+                    )
+                # female
+                else:
+                    ped_data["inferred"]["color"].append("rgba(227,26,28,0.5)")
+                    ped_data["inferred"]["hover"].append(
+                        "Sample: %s<br>Inferred X CN: 2" % (row[sample_col],)
+                    )
+
+                # bin plot
+                total = row["bins.in"] + row["bins.out"]
+                ped_data["bins"]["samples"].append(row[sample_col])
+                ped_data["bins"]["x"].append(row["bins.lo"] / total)
+                ped_data["bins"]["y"].append(row["bins.out"] / total)
+
+                # PCAs
+                try:
+                    ped_data["pca"]["pca_1"].append(row["PC1"])
+                    ped_data["pca"]["pca_2"].append(row["PC2"])
+                    ped_data["pca"]["pca_3"].append(row["PC3"])
+                except KeyError:
+                    pass
 
     traces["ped"] = table_data
     traces["sample_column"] = sample_col
-    traces["depth"] = ped_data
+    if via_indexcov:
+        traces["depth"] = ped_data
     return traces
