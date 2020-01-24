c3a9d3e9472107dd6f8a932574773b6d6b25e2f4
Joe Brown <brwnjm@gmail.com>
2020-01-24 13:00:30 -0700

    adds colors

--- covviz/utils.py
+++ covviz/utils.py
@@ -1,5 +1,39 @@
 import gzip
 
+# 3 sets of 10 colors
+COLORS = [
+    "#1f77b4",  # plotly and d3
+    "#ff7f0e",
+    "#2ca02c",
+    "#d62728",
+    "#9467bd",
+    "#8c564b",
+    "#e377c2",
+    "#7f7f7f",
+    "#bcbd22",
+    "#17becf",
+    "#7CB5EC",  # highcharts
+    "#434348",
+    "#90ED7D",
+    "#F7A35C",
+    "#8085E9",
+    "#F15C80",
+    "#E4D354",
+    "#2B908F",
+    "#F45B5B",
+    "#91E8E1",
+    "#4E79A7",  # tableau
+    "#F28E2C",
+    "#E15759",
+    "#76B7B2",
+    "#59A14F",
+    "#EDC949",
+    "#AF7AA1",
+    "#FF9DA7",
+    "#9C755F",
+    "#BAB0AB",
+]
+
 
 def gzopen(f):
     if f.endswith(".gz"):
